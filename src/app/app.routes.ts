7ce334be28092b0faf77cc55daeba12aa66b3eaf
Gaston Gallegos <gaston52343@gmail.com>
2024-05-28 18:46:43 -0300

    add 404 page, add Firebase auth guard to auth module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
+import { AuthGuard, redirectLoggedInTo } from '@angular/fire/auth-guard';
+import { Page404Component } from './modules/shared/pages/page404/page404.component';
 
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 export const routes: Routes = [
-    //Temporal until main view implemented
+
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
     {
         path: 'auth',
-        loadChildren: () => import('./modules/auth/auth.routes').then(m => m.authRoutes)
+        loadChildren: () => import('./modules/auth/auth.routes').then(m => m.authRoutes),
+        canActivate: [AuthGuard],
+        data: { authGuardPipe: redirectLoggedInToHome },
+    },
+    {
+        path: 'home',
+        component: Page404Component
+        // loadComponent: ()=> import('./modules/shared/pages/page404').then(m => m.Page404Component),
+        // loadChildren: () => import('./modules/auth/auth.routes').then(m => m.authRoutes),
+        // canActivate: [AuthGuard]
     },
     { path: '**', redirectTo: 'auth' }
 ];
